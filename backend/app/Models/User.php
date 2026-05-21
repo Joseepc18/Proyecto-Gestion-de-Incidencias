96eb4f88308c31604a4d189afeddc1aec8cbf018
Dayron Quiñonez Valencia <dayronqvalencia2004@gmail.com>
2026-05-21 17:53:03 -0500

    refactor: actualizar modelo User
    Se modificó el archivo User.php para adaptar los campos y asegurar la relación con el modelo Rol.

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -3,13 +3,14 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable, HasApiTokens;
+    use HasFactory, Notifiable, HasApiTokens, SoftDeletes;
 
     protected $table = 'users';
 
@@ -36,5 +37,40 @@ class User extends Authenticatable
     public function rol()
     {
         return $this->belongsTo(Rol::class, 'id_rol', 'id_rol');
+    }
+
+    public function incidencias()
+    {
+        return $this->hasMany(Incidencia::class, 'id_usuario', 'id');
+    }
+
+    public function asignaciones()
+    {
+        return $this->hasMany(AsignacionIncidencia::class, 'id_usuario', 'id');
+    }
+
+    public function comentarios()
+    {
+        return $this->hasMany(Comentario::class, 'id_usuario', 'id');
+    }
+
+    public function evidencias()
+    {
+        return $this->hasMany(Evidencia::class, 'id_usuario', 'id');
+    }
+
+    public function notificaciones()
+    {
+        return $this->hasMany(Notificacion::class, 'id_usuario', 'id');
+    }
+
+    public function historialEstados()
+    {
+        return $this->hasMany(HistorialEstado::class, 'id_usuario', 'id');
+    }
+
+    public function bitacoraErrores()
+    {
+        return $this->hasMany(BitacoraError::class, 'id_usuario', 'id');
     }
 }
